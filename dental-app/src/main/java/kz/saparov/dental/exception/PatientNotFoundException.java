22ffdf3537ffca5729f3a3fa204c21d8e1642b0a
Meiram <saparoffmeiram@yandex.ru>
2021-05-01 11:18:35 +0600

    add response status

--- dental-app/src/main/java/kz/saparov/dental/exception/PatientNotFoundException.java
+++ dental-app/src/main/java/kz/saparov/dental/exception/PatientNotFoundException.java
@@ -1,10 +1,9 @@
 package kz.saparov.dental.exception;
 
-public class PatientNotFoundException extends Exception {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class PatientNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
-
-	public PatientNotFoundException(String message) {
-		super(message);
-	}
 }
