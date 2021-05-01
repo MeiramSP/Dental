a3b0752807043367664f8a1080a262e8b8518594
Meiram <saparoffmeiram@yandex.ru>
2021-05-01 11:11:17 +0600

    change mapping from class to methods

--- dental-app/src/main/java/kz/saparov/dental/controller/AppointmentController.java
+++ dental-app/src/main/java/kz/saparov/dental/controller/AppointmentController.java
@@ -1,45 +1,33 @@
 package kz.saparov.dental.controller;
 
-import org.springframework.http.ResponseEntity;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kz.saparov.dental.entity.Appointment;
-import kz.saparov.dental.exception.AppointmentsNotFoundException;
-import kz.saparov.dental.exception.PatientNotFoundException;
 import kz.saparov.dental.service.AppointmentService;
 import kz.saparov.dental.util.AppointmentFinder;
 
 @RestController
-@RequestMapping("/appointments")
 public class AppointmentController {
 	
 	private AppointmentService appointmentService;
-
+	
 	public AppointmentController(AppointmentService appointmentService) {
 		this.appointmentService = appointmentService;
 	}
 	
-	@PostMapping("/{id}")
-	public ResponseEntity<String> saveAppointment(@PathVariable Long id, @RequestBody Appointment appointment) {
-		try {
-			appointmentService.addAppointment(id, appointment);
-			return ResponseEntity.ok("Запись на прием добавлена");
-		} catch (PatientNotFoundException e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
+	@PostMapping("/appointments/{id}")
+	public String saveAppointment(@PathVariable Long id, @RequestBody Appointment appointment) {
+		appointmentService.addAppointment(id, appointment);
+		return "Запись на прием добавлена";
 	}
 	
-	@PostMapping
-	public ResponseEntity<Object> findRecords(@RequestBody AppointmentFinder finder) {
-		try {
-			return ResponseEntity.ok(appointmentService.findByPeriod(finder));
-		} catch (AppointmentsNotFoundException e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
-		
+	@PostMapping("/appointments")
+	public List<Appointment> findRecords(@RequestBody AppointmentFinder finder) {
+		return appointmentService.findByPeriod(finder);
 	}
 }
--- dental-app/src/main/java/kz/saparov/dental/controller/PatientController.java
+++ dental-app/src/main/java/kz/saparov/dental/controller/PatientController.java
@@ -1,7 +1,7 @@
 package kz.saparov.dental.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,67 +9,47 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kz.saparov.dental.entity.Patient;
-import kz.saparov.dental.exception.PatientNotFoundException;
 import kz.saparov.dental.service.PatientService;
 
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/patients")
 public class PatientController {
 	
 	private PatientService patientService;
 
-	@Autowired
 	public PatientController(PatientService patientService) {
 		this.patientService = patientService;
 	}
 	
-	@PostMapping
-	public ResponseEntity<String> savePatient(@RequestBody Patient patient) {
-		try {
-			patientService.addPatient(patient);
-			return ResponseEntity.ok("Пациент успешно добавлен");
-			
-		} catch (Exception e) {
-			return ResponseEntity.badRequest().body("Произошла ошибка");
-		}
+	@PostMapping("/patients")
+	public Patient savePatient(@RequestBody Patient patient) {
+		return patientService.addPatient(patient);	
 	}
 	
-	@GetMapping
-	public ResponseEntity<Object> findPatient(@RequestParam Long id) {
-		try {
-			return ResponseEntity.ok(patientService.getPatient(id));
-		} catch (PatientNotFoundException e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		} catch (Exception e) {
-			return ResponseEntity.badRequest().body("Произошла ошибка");
-		}
+	@GetMapping("/patients/{id}")
+	public Patient findPatient(@PathVariable Long id) {
+		return patientService.getPatient(id);
 	}
 	
-	@PutMapping("/{id}")
-	public ResponseEntity<String> updatePatient(@PathVariable Long id, @RequestBody Patient patient) {
-		try {
-			patientService.updatePatient(id, patient);
-			return ResponseEntity.ok("Данные о пациенте успешно обновлены");
-		} catch (PatientNotFoundException e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		} catch (Exception e) {
-			return ResponseEntity.badRequest().body("Произошла ошибка");
-		}
+	@PutMapping("/patients/{id}")
+	public String updatePatient(@PathVariable Long id, @RequestBody Patient patient) {
+		patientService.updatePatient(id, patient);
+		return "Данные успешно обновлены";
 	}
 	
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Object> deletePatient(@PathVariable Long id) {
-		try {
-			return ResponseEntity.ok(patientService.deletePatient(id));
-		}  catch (Exception e) {
-			return ResponseEntity.badRequest().body("Произошла ошибка");
-		}
+	@DeleteMapping("/patients/{id}")
+	public String deletePatient(@PathVariable Long id) {
+		patientService.deletePatient(id);
+		return "Пациент успешно удален";
+	}
+	
+	@GetMapping("/patients") 
+	public List<Patient> searchPatient(@RequestParam String param) {
+		return patientService.searchPatient(param);
 	}
 	
 }
